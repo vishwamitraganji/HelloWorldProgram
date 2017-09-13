0620d4999f3fd8046bbc0e774dd0e2a6a38f3d9c
vishwamitraganji <31531257+vishwamitraganji@users.noreply.github.com>
2017-09-13 14:42:34 +0530

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World again!" );
         System.exit( 0 ); //success
     }
 }
